bbaf16218e8254ed46ef4b29882fad1c40554542
Owen Watson <owen@thewatson.co.uk>
2025-11-13 06:07:28 +0000

    Limited number of home page posts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,13 +14,26 @@ export class HomeComponent {
   articles: Article[] = [];
   errorMessage!: string;
 
-  constructor(private service: PurpleFenixApi) { }
+  constructor() { }
 
   ngOnInit() {
-    this.articles = getIndexOfArticles();
+    this.articles = this.getLastNElements(getIndexOfArticles(), 5);
   }
 
   parseDateString(date: Date): string {
-    return formatDate(date,'MMMM dd yyyy','en-US');
+    return formatDate(date, 'MMMM dd yyyy', 'en-US');
   }
+
+  // Function to get the last N elements from an array
+  getLastNElements<T>(arr: T[], n: number): T[] {
+    if (!Array.isArray(arr)) {
+      throw new Error("First argument must be an array");
+    }
+    if (typeof n !== "number" || n < 0) {
+      throw new Error("Number of elements must be a non-negative integer");
+    }
+    // slice(-n) returns the last n elements; handles n > arr.length automatically
+    return arr.slice(-n);
+  }
+
 }
